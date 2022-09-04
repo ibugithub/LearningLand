251a41680787c817176ba7a7a5ded8b3f4e91ae0
Ibrahim Hossain <outsideworkibrahim@gmail.com>
2022-09-04 11:57:51 +0600

    added file for java and and screenshoot of linux and devops commands

---
+++ Learning Java/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+
+	public static void main (String[] args) {
+		
+		System.out.println("hello World I'm ibu waiting for someone..I really loved somone but she hated me so much also");
+	
+	}
+
+}
---
+++ Learning Java/Java/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        byte b = 1;
+        System.out.println(b);
+        float f = 1.2f;
+        double d = 34.34;
+        long l = 1000000000;
+
+        }
+    }
+
+
